0ce29fe5e582cf3dbac7593c8f86f2fde249d122
Mauricio Laine <laine.mau@gmail.com>
2019-11-18 20:36:03 -0800

    checked a better way to instantiate strings

--- src/chap2_creating_and_destroying_obj/item5_avoid_creating_unnecessary_obj/TestUnnecessaryObjects.java
+++ src/chap2_creating_and_destroying_obj/item5_avoid_creating_unnecessary_obj/TestUnnecessaryObjects.java
@@ -4,6 +4,22 @@ public class TestUnnecessaryObjects {
 
     public static void main(String[] args) {
 
+        //*** Avoid creating unnecessary objects ***
+
+        testStrings();
     }
 
+    static void testStrings() {
+
+        //DON'T DO THIS!
+        //The statement creates a new String instance each time it is executed, and none of those
+        // object creations is necessary
+        final String garfield = new String("Garfield");
+
+        //This version uses a single String instance, rather than creating a new one each time
+        // it is executed
+        final String silvestre = "Silvestre";
+
+        System.out.println("cats: " + garfield + " & " + silvestre);
+    }
 }
