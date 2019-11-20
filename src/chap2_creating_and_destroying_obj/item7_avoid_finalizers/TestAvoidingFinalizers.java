e2985845acc56a350161abe17034e86443086e8c
Mauricio Laine <laine.mau@gmail.com>
2019-11-20 10:51:38 -0800

    created an item7 package and the test class

---
+++ src/chap2_creating_and_destroying_obj/item7_avoid_finalizers/TestAvoidingFinalizers.java
@@ -0,0 +1,12 @@
+package chap2_creating_and_destroying_obj.item7_avoid_finalizers;
+
+public class TestAvoidingFinalizers {
+
+    public static void main(String[] args) {
+
+        //*** Avoid finalizers ***
+
+        //Finalizers are unpredictable, often dangerous, and generally unnecessary
+
+    }
+}
