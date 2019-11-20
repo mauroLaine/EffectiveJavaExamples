281cb191af9aed3e872bf9b2e4b9ff1b641d0f90
Mauricio Laine <laine.mau@gmail.com>
2019-11-20 11:53:30 -0800

    created the chapter 3 package, and the item 8 and 9 packages

---
+++ src/chap3_methods_common_to_all_obj/item8_obey_general_contract_overriding_equals/TestOverridingEquals.java
@@ -0,0 +1,10 @@
+package chap3_methods_common_to_all_obj.item8_obey_general_contract_overriding_equals;
+
+public class TestOverridingEquals {
+
+    public static void main(String[] args) {
+
+        //*** Obey the general contract when overriding equals ***
+
+    }
+}
