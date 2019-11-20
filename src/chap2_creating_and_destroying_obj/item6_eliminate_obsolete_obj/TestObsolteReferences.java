e9c46fefa89f02fd345c2ea76a1b6c56e5558044
Mauricio Laine <laine.mau@gmail.com>
2019-11-20 10:23:59 -0800

    added a text file, and reading the content to make an obsolete reference example

--- src/chap2_creating_and_destroying_obj/item6_eliminate_obsolete_obj/TestObsolteReferences.java
+++ src/chap2_creating_and_destroying_obj/item6_eliminate_obsolete_obj/TestObsolteReferences.java
@@ -1,9 +1,19 @@
 package chap2_creating_and_destroying_obj.item6_eliminate_obsolete_obj;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class TestObsolteReferences {
 
     public static void main(String[] args) {
 
-
+        try {
+            final BufferedReader br = new BufferedReader(new FileReader("assets/test_text.txt"));
+            while (br.readLine() != null) {
+                System.out.println(br.readLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
