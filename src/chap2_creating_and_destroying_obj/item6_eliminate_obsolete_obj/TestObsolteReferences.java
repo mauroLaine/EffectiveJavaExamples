68fc77ecad14a57eaa09be54e7dd5deceff5956c
Mauricio Laine <laine.mau@gmail.com>
2019-11-20 10:48:49 -0800

    added the example from the book, and set comments in the test class

--- src/chap2_creating_and_destroying_obj/item6_eliminate_obsolete_obj/TestObsolteReferences.java
+++ src/chap2_creating_and_destroying_obj/item6_eliminate_obsolete_obj/TestObsolteReferences.java
@@ -2,18 +2,66 @@ package chap2_creating_and_destroying_obj.item6_eliminate_obsolete_obj;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.EmptyStackException;
 
 public class TestObsolteReferences {
 
     public static void main(String[] args) {
 
+        //*** Eliminate obsolete object references ***
+
+        // - Generally speaking, whenever a class manages its own memory, the programmer should be alert
+        //      for memory leaks
+        // - Nulling out object references should be the exception rather than the norm
+        // - Another common source of memory leaks is caches
+        // - A third common source of memory leaks is listeners and other callbacks
+
         try {
-            final BufferedReader br = new BufferedReader(new FileReader("assets/test_text.txt"));
-            while (br.readLine() != null) {
-                System.out.println(br.readLine());
+            //Read the memory leak definition in memory_leak.txt
+            final BufferedReader br = new BufferedReader(new FileReader("assets/memory_leak.txt"));
+            try {
+                while (br.readLine() != null) {
+                    System.out.println(br.readLine());
+                }
+                Thread.sleep(500000);
+            } finally {
+                br.close(); //close the buffer
             }
-        } catch (Exception e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    static class Stack {
+        private Object[] elements;
+        private int size = 0;
+        private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+        public Stack() {
+            elements = new Object[DEFAULT_INITIAL_CAPACITY];
+        }
+
+        public void push(Object e) {
+            ensureCapacity();
+            elements[size++] = e;
+        }
+
+        public Object pop() {
+            if (size == 0)
+                throw new EmptyStackException();
+            // this creates a Memory leak
+            //return elements[--size];
+
+            Object result = elements[--size];
+            elements[size] = null; // Eliminate obsolete reference
+            return result;
+        }
+
+        private void ensureCapacity() {
+            if (elements.length == size)
+                elements = Arrays.copyOf(elements, 2 * size + 1);
         }
     }
 }
